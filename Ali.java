bfbe015f7262cc0c33da85fb467dc9936919cd35
Ali Hassan <alihassan057689234@gmail.com>
2024-11-23 15:07:59 -0800

    packages in java

---
+++ Ali.java
@@ -0,0 +1,12 @@
+package Package1;
+
+public class Ali {
+   private int x,y;
+   public void setData(int a,int b){
+    x = a; y = b;
+   } 
+   public void showData(){
+    System.out.println("Value of x = "+x);
+    System.out.println("Value of y = "+y);
+   }
+}
---
+++ Azhar.java
@@ -0,0 +1,10 @@
+package Package2;
+import Package1.*;
+
+class Azhar {
+   public static void main(String[] args) {
+    Ali obj = new Ali();
+    obj.setData(5, 6);
+    obj.showData();
+   } 
+}
