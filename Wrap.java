e8be474aa2c4c5fcaa00245b5cf9e9e661dce3e6
Ali Hassan <alihassan057689234@gmail.com>
2024-11-23 13:26:45 -0800

    wrap class in java

---
+++ Wrap.java
@@ -0,0 +1,10 @@
+class Wrap {
+     public static void main(String []args){
+        // convert into integer
+        int x = Integer.parseInt("123");
+        // create objectq
+        Integer y = Integer.valueOf("10010",2);
+        System.out.println(x);
+        System.out.println(y);
+     }    
+}
